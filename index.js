af8928625aec7b1e6817aaee5f474ebc76aa3bf7
Armoru <rmoru31@gmail.com>
2018-04-25 15:03:07 +0700

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+console.log("hello weld");
+process.env.TZ = 'Asia/Jakarta';
+let date = new Date();
+let hr = date.getHours();
+let day= date.getDay();
+
+bot.on('message',(message)=>{
+    if(message.content=='!check'){
+        message.reply("I'ts"+ hr + " "+day);
+    }
+});
+
+var schedule = require('node-schedule');
+ 
+var j = schedule.scheduleJob('0 * * * * *', function(){
+    message.channel.send('@everyone Time to hit the #Gym');
+});
+
+/*var j = schedule.scheduleJob('00 00 20 * * *', function(){
+    message.channel.send('@everyone Time to hit the #Gym');
+});*/
+
+bot.login(process.env.BOT_TOKEN);
