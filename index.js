3a7419640f4277509b16ec9701fba455ab7170d3
Armoru <rmoru31@gmail.com>
2018-04-26 10:02:30 +0700

    test12

--- index.js
+++ index.js
@@ -10,22 +10,24 @@ let day= date.getDay();
 bot.on('message',(message)=>{
     if(message.content=='!check'){
         message.reply("I'ts"+ hr + " "+day);
+    }else if(message.content=='!start'){
+        var schedule = require('node-schedule');
+
+        var j = schedule.scheduleJob('10 * * * * *', function(){
+            console.log("@everyone Time to hit the #Gym");
+            message.channel.send("@everyone Time to hit the #Gym");
+        });
     }
 });
 
-bot.on('ready',()=>{
-    var schedule = require('node-schedule');
-
-    var j = schedule.scheduleJob('10 * * * * *', function(){
-        console.log("@everyone Time to hit the #Gym");
-        const channel = bot.channels.get('432870102178988032');
-        channel.send("@everyone Time to hit the #Gym");
+/*bot.on('ready',()=>{
+    
         
         //how fix this wtf//var channel = Guild.Client .get("name", "The Castle").defaultChannel;
         //bot.sendMessage(channel, "@everyone Time to hit the #Gym");
         //432870102178988032
     });
-});
+});*/
 
     
 
